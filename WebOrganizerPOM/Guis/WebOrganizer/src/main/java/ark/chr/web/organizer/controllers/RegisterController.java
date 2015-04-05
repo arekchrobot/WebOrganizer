e9075c533917e0e713123897f27e651518f33083
Arkadiusz Chrobot <chrobot.arkadiusz@gmail.com>
2015-04-05 09:29:16 +0200

    Added success page for registration and error message for wrong email, error page for logging

--- WebOrganizerPOM/Guis/WebOrganizer/src/main/java/ark/chr/web/organizer/controllers/RegisterController.java
+++ WebOrganizerPOM/Guis/WebOrganizer/src/main/java/ark/chr/web/organizer/controllers/RegisterController.java
@@ -2,6 +2,9 @@ package ark.chr.web.organizer.controllers;
 
 import ark.chr.web.organizer.model.OrganizerUser;
 import ark.chr.web.organizer.services.api.IRegisterService;
+import java.util.ResourceBundle;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.slf4j.Logger;
@@ -17,16 +20,17 @@ import org.springframework.context.annotation.Scope;
 public class RegisterController {
 
     private static final Logger logger = LoggerFactory.getLogger(RegisterController.class);
-    
+    private static final String REGISTER_SUCCESS = "registerSuccess";
+
     @Inject
     private IRegisterService registerService;
-    
+
     private OrganizerUser registerUser;
 
     public RegisterController() {
         registerUser = new OrganizerUser();
     }
-    
+
     public OrganizerUser getRegisterUser() {
         return registerUser;
     }
@@ -34,12 +38,20 @@ public class RegisterController {
     public void setRegisterUser(OrganizerUser registerUser) {
         this.registerUser = registerUser;
     }
-    
+
     public String registerNewUser() {
         logger.info("Registering new user: " + registerUser.getLogin());
-        if(registerService.registerUser(registerUser)) {
-            return "OK";
+        if (registerService.registerUser(registerUser)) {
+            return REGISTER_SUCCESS;
         }
-        return "FAIL";
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ResourceBundle bundle
+                = facesContext.getApplication().getResourceBundle(
+                        facesContext, "myMessages");
+        facesContext.addMessage(null, 
+                new FacesMessage(FacesMessage.SEVERITY_FATAL,
+                        bundle.getString("register.error.message.header"),
+                        bundle.getString("register.error.message.detail")));
+        return null;
     }
 }
